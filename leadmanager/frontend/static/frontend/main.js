ba0892828e904012595cc2532b7d0eeb585c1170
Jason <jasonetienne1@gmail.com>
2022-10-17 20:10:40 +0100

    updated html, H1 not returing on app, need to sort

---
+++ leadmanager/frontend/static/frontend/main.js
@@ -0,0 +1,108 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./leadmanager/frontend/src/components/App.js":
+/*!****************************************************!*\
+  !*** ./leadmanager/frontend/src/components/App.js ***!
+  \****************************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\leadmanager\\\\frontend\\\\src\\\\components\\\\App.js: Identifier 'React' has already been declared. (2:7)\\n\\n\\u001b[0m \\u001b[90m 1 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReact\\u001b[39m\\u001b[33m,\\u001b[39m { \\u001b[33mComponent\\u001b[39m } \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"react\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 2 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReact\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m\\\"react-dom\\\"\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m        \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m \\u001b[36mclass\\u001b[39m \\u001b[33mApp\\u001b[39m \\u001b[36mextends\\u001b[39m \\u001b[33mComponent\\u001b[39m {\\u001b[0m\\n    at instantiate (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:72:32)\\n    at constructor (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:367:12)\\n    at JSXParserMixin.raise (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3706:19)\\n    at ScopeHandler.checkRedeclarationInScope (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1657:19)\\n    at ScopeHandler.declareName (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:1623:12)\\n    at JSXParserMixin.declareNameFromIdentifier (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12375:16)\\n    at JSXParserMixin.checkIdentifier (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12370:12)\\n    at JSXParserMixin.checkLVal (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12296:12)\\n    at JSXParserMixin.finishImportSpecifier (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16590:10)\\n    at JSXParserMixin.parseImportSpecifierLocal (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16586:31)\\n    at JSXParserMixin.maybeParseDefaultImportSpecifier (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16702:12)\\n    at JSXParserMixin.parseImport (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16550:31)\\n    at JSXParserMixin.parseStatementContent (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14981:27)\\n    at JSXParserMixin.parseStatement (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14871:17)\\n    at JSXParserMixin.parseBlockOrModuleBlockBody (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:15531:25)\\n    at JSXParserMixin.parseBlockBody (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:15522:10)\\n    at JSXParserMixin.parseProgram (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14785:10)\\n    at JSXParserMixin.parseTopLevel (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:14772:25)\\n    at JSXParserMixin.parse (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16799:10)\\n    at parse (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:16851:38)\\n    at parser (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\parser\\\\index.js:52:34)\\n    at parser.next (<anonymous>)\\n    at normalizeFile (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\normalize-file.js:87:38)\\n    at normalizeFile.next (<anonymous>)\\n    at run (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transformation\\\\index.js:31:50)\\n    at run.next (<anonymous>)\\n    at transform (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\transform.js:29:41)\\n    at transform.next (<anonymous>)\\n    at step (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\User\\\\Documents\\\\Coding\\\\FullStack_React_-_Django_App\\\\node_modules\\\\gensync\\\\index.js:223:11)\");\n\n//# sourceURL=webpack://fullstack_react_-_django_app/./leadmanager/frontend/src/components/App.js?");
+
+/***/ }),
+
+/***/ "./leadmanager/frontend/src/index.js":
+/*!*******************************************!*\
+  !*** ./leadmanager/frontend/src/index.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/App */ \"./leadmanager/frontend/src/components/App.js\");\n/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_App__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack://fullstack_react_-_django_app/./leadmanager/frontend/src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./leadmanager/frontend/src/index.js");
+/******/ 	
+/******/ })()
+;
